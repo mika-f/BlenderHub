a9af93653820bfd01d7422483fdfa8e193d80390
Natsuneko <me@mochizuki.moe>
2021-04-25 12:08:10 +0900

    feat: detect platform

--- src/main/platform.ts
+++ src/main/platform.ts
@@ -1,3 +1,20 @@
 type Platform = "win" | "macOS" | "linux";
 
+const getPlatform = (): Platform => {
+  switch (process.platform) {
+    case "darwin":
+      return "macOS";
+
+    case "win32":
+      return "win";
+
+    case "linux":
+      return "linux";
+
+    default:
+      throw new Error("Unsupported Platform");
+  }
+};
+
 export type { Platform };
+export { getPlatform };
