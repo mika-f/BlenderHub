86868d28f984de1e4c351b6c41353bfeba9a7592
Natsuneko <me@mochizuki.moe>
2021-04-25 12:08:38 +0900

    feat: state structure

--- src/renderer/state/global.ts
+++ src/renderer/state/global.ts
@@ -1,13 +1,23 @@
 import { createGlobalState } from "react-hooks-global-state";
 
+import type { Installations } from "renderer/messaging/fetcher";
+
 type State = {
-  title: string;
-  isShowPrevious: boolean;
+  installations: {
+    stable: { versions: Installations };
+    daily: { versions: Installations };
+    experimental: { versions: Installations };
+  };
+  libraryPath?: string;
 };
 
 const { useGlobalState } = createGlobalState<State>({
-  title: "Blender Hub",
-  isShowPrevious: false,
+  installations: {
+    stable: { versions: [] },
+    daily: { versions: [] },
+    experimental: { versions: [] },
+  },
+  libraryPath: undefined,
 });
 
 export { useGlobalState };
