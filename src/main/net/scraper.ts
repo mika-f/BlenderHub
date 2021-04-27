bc83e47a7fcc7e282f0fa21f627481995e1f7042
Natsuneko <me@mochizuki.moe>
2021-04-28 00:20:11 +0900

    feat: support experimental builds

--- src/main/net/scraper.ts
+++ src/main/net/scraper.ts
@@ -43,7 +43,7 @@ const fetchDailyReleases = async (platform: Platform): Promise<Version[]> => {
     const html = response.body;
 
     const $ = cheerio.load(html);
-    $("a", `section.platform-${platform}`).each((_i1, element) => {
+    $("a", `section.platform-${platform}`).each((_, element) => {
       const elem = $(element);
       const url = elem.attr("href");
       const version = $("span.name", elem)
@@ -60,8 +60,27 @@ const fetchDailyReleases = async (platform: Platform): Promise<Version[]> => {
   return versions;
 };
 
-const fetchExperimentalReleases = async (_platform: Platform): Promise<Version[]> => {
+const fetchExperimentalReleases = async (platform: Platform): Promise<Version[]> => {
   const versions: Version[] = [];
+
+  try {
+    const response = await got("https://builder.blender.org/download/branches/");
+    const html = response.body;
+
+    const $ = cheerio.load(html);
+    $("a", `section.platform-${platform}`).each((_, element) => {
+      const elem = $(element);
+      const url = elem.attr("href");
+      const version = $("span.name", elem)
+        .text()
+        .match(/Blender\s(?<version>\d+\.\d+(\.\d+)?\s.*)\sbranch/)!.groups!.version!;
+
+      versions.push({ version, url: `https://builder.blender.org/download/branches${url}`, branch: "experimental" });
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
 
   return versions;
 };
